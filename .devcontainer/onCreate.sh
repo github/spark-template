eb0cd10d834682835013bb93422f6b63bfd2641d
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-04-18 22:15:19 +0000

    Initial commit

---
+++ .devcontainer/onCreate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+rm -rf ./spark-sdk-dist
+rm -rf /workspaces/spark-tools
+
+echo "Downloading the latest release of workbench-template from GitHub"
+
+GITHUB_PAT="$RELEASE_PAT"
+REPO="github/workbench-template"
+
+# Fetch the latest release information
+LATEST_RELEASE=$(curl -s -H "Authorization: token $GITHUB_PAT" https://api.github.com/repos/$REPO/releases/latest)
+
+# Extract the first browser_download_url from the assets
+DOWNLOAD_URL=$(echo "$LATEST_RELEASE" | jq -r '.assets[0].url')
+echo "Download URL: $DOWNLOAD_URL"
+
+
+# Fetch the latest release information
+curl -L -o dist.zip -H "Authorization: token $GITHUB_PAT" -H "Accept: application/octet-stream" "$DOWNLOAD_URL"
+
+unzip -o dist.zip
+rm dist.zip
+
+sudo mv ./spark-sdk-dist/server.js /usr/local/bin/spark-server
+sudo mv ./spark-sdk-dist/designer.js /usr/local/bin/spark-designer
+sudo mv ./spark-sdk-dist/file-syncer.js /usr/local/bin/spark-file-syncer
+sudo mv ./spark-sdk-dist/spark-agent.js /usr/local/bin/spark-agent
+
+tar -xzf ./spark-sdk-dist/spark-tools.tgz
+
+mkdir -p /workspaces/spark-tools
+sudo mv ./package/* /workspaces/spark-tools
+sudo rmdir ./package
+rm -rf ./spark-sdk-dist
+
+npm i -f /workspaces/spark-tools
---
+++ vite.config.ts
@@ -0,0 +1,90 @@
+// IMPORTANT NOTE: This file is only used in two situations: local development, and the live preview in Workbench.
+// For deployed Sparks, the `server/main.ts` serves the app itself. Ensure consistency between this file and `server/main.ts`
+// if you have something that needs to available while deployed.
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, PluginOption } from "vite";
+
+import { createLogToFileLogger } from "@github/spark/logToFileLogger";
+import { runtimeTelemetryPlugin } from "@github/spark/telemetryPlugin";
+import sparkAgent from "./spark-agent/src/server/vite-plugin";
+import { designerHost, tagSourcePlugin } from "./spark-designer/plugins";
+import createIconImportProxy from "@github/spark/vitePhosphorIconProxyPlugin";
+
+const extraPlugins: PluginOption[] = [];
+
+if (process.env.USE_DESIGNER === "true") {
+  // Even though these types are exactly the type of our array, technically the types are coming
+  // from different versions of the same package, so we need to cast them to **this** package's type.
+  // Notice that it's coming from the `spark-designer` folder, which is a different package
+  // that includes its own version of `vite`.
+  extraPlugins.push([tagSourcePlugin() as PluginOption, designerHost() as PluginOption]);
+}
+
+if (process.env.USE_SPARK_AGENT === "true") {
+  // See above comment about the type casting.
+  extraPlugins.push(sparkAgent({ serverURL: process.env.SPARK_AGENT_URL }) as PluginOption);
+}
+
+const GITHUB_RUNTIME_PERMANENT_NAME = process.env.GITHUB_RUNTIME_PERMANENT_NAME || process.env.CODESPACE_NAME?.substring(0, 20);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    createIconImportProxy(),
+    react(),
+    tailwindcss(),
+    runtimeTelemetryPlugin(),
+    ...extraPlugins,
+  ],
+  define: {
+    // ensure that you give these types in `src/vite-end.d.ts`
+    GITHUB_RUNTIME_PERMANENT_NAME: JSON.stringify(GITHUB_RUNTIME_PERMANENT_NAME),
+    BASE_KV_SERVICE_URL: JSON.stringify("/kv"),
+  },
+  server: {
+    port: 5000,
+    hmr: {
+      overlay: false,
+    },
+    watch: {
+      ignored: ["**/prd.md", "**.log"],
+      awaitWriteFinish: {
+        pollInterval: 50,
+        stabilityThreshold: 100,
+      },
+    },
+    warmup: {
+      clientFiles: [
+        "./src/App.tsx",
+        "./src/index.css",
+        "./index.html",
+        "./src/**/*.tsx",
+        "./src/**/*.ts",
+        "./src/**/*.jsx",
+        "./src/**/*.js",
+        "./src/styles/theme.css",
+      ],
+    },
+    proxy: {
+      // Any new endpoints defined in the backend server need to be added here
+      // as vite serves the frontend during local development and in the live preview,
+      // and needs to know to proxy the endpoints to the backend server.
+      "/kv": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+      "/llm": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/workspaces/workbench-template/src"
+    }
+  },
+  customLogger: createLogToFileLogger(),
+});
