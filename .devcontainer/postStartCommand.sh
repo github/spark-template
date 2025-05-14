1b3bda22907d71bdef71d00324fb8eee697dc33f
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-05-14 18:21:09 -0400

    fix log chown (#37)

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 sudo cp .devcontainer/spark.conf /etc/supervisor/conf.d/
 
 sudo chown node /var/run/
-sudo chown node /var/log/
+sudo chown -R node /var/log/
 
 supervisord
 supervisorctl reread
