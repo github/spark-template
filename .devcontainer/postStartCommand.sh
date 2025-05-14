b9fa25e8bff8b96485574ecada35c7d74e25e2c8
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-05-14 20:44:42 +0000

    fix supervisord call

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -8,6 +8,6 @@ sudo cp .devcontainer/spark.conf /etc/supervisor/conf.d/
 sudo chown $current_user /var/run/supervisor.sock
 sudo chown $current_user /var/log/supervisor
 
-supervisor start
+supervisord
 supervisorctl reread
 supervisorctl update
