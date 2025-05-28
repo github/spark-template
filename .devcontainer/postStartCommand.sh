71cb3619f40b55176c2894ef7ef13ca9f21cb92e
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-05-28 16:29:59 +0000

    hydrate

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,6 +3,12 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 "$SCRIPT_DIR/refreshTools.sh"
 
+# Check if SNAPSHOT_SAS_URL was passed, if so run hydrate.sh in background
+if [ -n "$SNAPSHOT_SAS_URL" ]; then
+    WORKSPACE_DIR="/workspaces/spark-template"
+    SAS_URI="$SNAPSHOT_SAS_URL" /usr/local/bin/hydrate.sh $WORKSPACE_DIR > $WORKSPACE_DIR/hydration.log 2>&1 &
+fi
+
 sudo cp .devcontainer/spark.conf /etc/supervisor/conf.d/
 
 sudo chown node /var/run/
@@ -11,3 +17,5 @@ sudo chown -R node /var/log/
 supervisord
 supervisorctl reread
 supervisorctl update
+
+npm i
