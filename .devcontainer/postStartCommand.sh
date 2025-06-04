930371bfc2e4834c302a0914bf18d56fe355c7fd
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-06-04 11:21:37 -0400

    Hydrate repo from remote (#50)

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,6 +3,12 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 "$SCRIPT_DIR/refreshTools.sh"
 
+# Check if SNAPSHOT_SAS_URL was passed, if so run hydrate.sh in background
+if [ -n "$SNAPSHOT_SAS_URL" ]; then
+    WORKSPACE_DIR="/workspaces/spark-template"
+    SAS_URI="$SNAPSHOT_SAS_URL" /usr/local/bin/hydrate.sh $WORKSPACE_DIR
+fi
+
 sudo cp .devcontainer/spark.conf /etc/supervisor/conf.d/
 
 sudo chown node /var/run/
