0c6fd689a2c9fdf58af04e2092a61de5f451384b
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-06-05 19:20:14 -0400

    Remove heartbeat patch (#56)

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import App from './App.tsx'
 import "./main.css"
 import "./styles/theme.css"
 import "./index.css"
-
-if (import.meta.env.MODE == 'development') {
-    await import('@github/spark/initializeTelemetry');
-}
-
 import "@github/spark/spark"
 
 createRoot(document.getElementById('root')!).render(
