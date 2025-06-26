de19d2758dec04984949d6b1446f577cad7e19f7
Aaron Ballard <ansballard@github.com>
2025-06-26 14:27:44 -0600

    🎇 Swap destructured imports from lucide for direct imports per icon (#81)
    the template is using lucide-react icons, which in production is tree
    shaken. But in dev, they pull the full 1.23mb file, vs ~2kb per icon if
    they're referenced directly. Updating these references to be direct
    imports will save almost the entire 1.23mb, is still legible, and does
    not affect prod bundle size.
    
    |full icon bundle|x icon|
    |---|---|
    |1.23mb|2.72kb|

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -4,7 +4,8 @@ import { ComponentProps, createContext, useCallback, useContext, useEffect, useS
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react"
-import { ArrowLeft, ArrowRight } from "lucide-react"
+import ArrowLeft from "lucide-react/dist/esm/icons/arrow-left"
+import ArrowRight from "lucide-react/dist/esm/icons/arrow-right"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
