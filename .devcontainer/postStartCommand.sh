dd78998817126306b6a7e92c0fff3f71f1aa2773
Eljo George <eljog@github.com>
2025-06-30 22:26:14 +0000

    it's not in bg

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 "$SCRIPT_DIR/refreshTools.sh"
 
-# Check if SNAPSHOT_SAS_URL was passed, if so run hydrate.sh in background
+# Check if SNAPSHOT_SAS_URL was passed, if so run hydrate.sh
 if [ -n "$SNAPSHOT_SAS_URL" ]; then
     WORKSPACE_DIR="/workspaces/spark-template"
     SAS_URI="$SNAPSHOT_SAS_URL" /usr/local/bin/hydrate.sh $WORKSPACE_DIR
