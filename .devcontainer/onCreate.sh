94980b4d0e9d0e169c8f09c2dc4c21b982e02f5e
Greg Fisher <gnfisher@github.com>
2025-07-16 18:30:20 -0400

    Update onCreate.sh to install ripgrep and fd-find

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -3,14 +3,14 @@
 set -e
 
 echo "Installing the GitHub CLI"
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget rg -y)) \
   && sudo mkdir -p -m 755 /etc/apt/keyrings \
         && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
         && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
   && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
   && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
   && sudo apt update \
-  && sudo apt install gh inotify-tools -y
+  && sudo apt install gh inotify-tools ripgrep fd-find -y
 
 echo "Installing azcopy"
 
