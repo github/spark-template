3188e03fbaf5543c7d74568a9c24aeae8c5fbc5e
Chava Barboza <salvador-barboza@users.noreply.github.com>
2025-07-18 18:09:44 +0000

    fix

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LATEST_RELEASE=$(bash "$SCRIPT_DIR/refreshTools.sh")
 
+sudo cp .devcontainer/spark.conf /etc/supervisor/conf.d/
+
 cd /tmp/spark
 bash spark-sdk-dist/repair.sh
 LATEST_RELEASE="$LATEST_RELEASE" WORKSPACE_DIR="$WORKSPACE_DIR" bash /tmp/spark/spark-sdk-dist/install-tools.sh services
-
-sudo cp .devcontainer/spark.conf /etc/supervisor/conf.d/
 
 sudo chown node /var/run/
 sudo chown -R node /var/log/
